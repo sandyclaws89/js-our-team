cd08507bc762f3cdd40fa54cfde1e1771a72c045
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-07 15:13:59 +0100

    carico pagina

---
+++ script/js.js
@@ -0,0 +1,10 @@
+
+/*Viene fornito un layout di base. Come prima cosa nel file js definitevi un array di oggetti che rappresentano i membro del team (prendete i dati dallo screen fornito). Ogni membro deve avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+Prendendo come riferimento la card di esempio presente nell'html, stampiamo dinamicamente una card per ogni membro del team.
+BONUS:
+Utilizziamo gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team: cliccando sul pulsante "add" viene creato un nuovo oggetto, il quale viene inserito nell'array iniziale, e viene stampata una nuova card con tutte le informazioni inserite dall'utente.
+Consigli del giorno:
+Ragioniamo come sempre a step.
+Prima la logica in italiano e poi traduciamo in codice.
+console.log() è nostro amico.
+Quando un pezzo di codice funziona, chiediamoci se possiamo scomporlo in funzioni più piccole.*/
